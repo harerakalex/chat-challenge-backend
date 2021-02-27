1070e9afbcf116229891e9aee5920ee650b030a8
harerakalex <hareraloston@gmail.com>
2021-02-27 18:33:26 +0200

    Changes

--- src/routes.js
+++ src/routes.js
@@ -28,51 +28,6 @@ const routes = [
     path: '/message',
     handler: MessageController.getUserMessages,
   },
-  // {
-  //     method: 'GET',
-  //     path: /\/employee\/([0-9a-z]+)/,
-  //     handler: employeeController.show.bind(employeeController)
-  // },
-  // {
-  //     method: 'POST',
-  //     path: '/employee',
-  //     handler: employeeController.create.bind(employeeController)
-  // },
-  // {
-  //     method: 'PUT',
-  //     path: /\/employee\/([0-9a-z]+)/,
-  //     handler: employeeController.update.bind(employeeController)
-  // },
-  // {
-  //     method: 'DELETE',
-  //     path: /\/employee\/([0-9a-z]+)/,
-  //     handler: employeeController.delete.bind(employeeController)
-  // },
-  // {
-  //     method: 'POST',
-  //     path: '/project',
-  //     handler: projectController.create.bind(projectController)
-  // },
-  // {
-  //     method: 'GET',
-  //     path: '/project',
-  //     handler: projectController.index.bind(projectController)
-  // },
-  // {
-  //     method: 'GET',
-  //     path: /\/project\/([0-9a-z]+)/,
-  //     handler: projectController.show.bind(projectController)
-  // },
-  // {
-  //     method: 'PUT',
-  //     path: /\/project\/([0-9a-z]+)/,
-  //     handler: projectController.update.bind(projectController)
-  // },
-  // {
-  //     method: 'DELETE',
-  //     path: /\/project\/([0-9a-z]+)/,
-  //     handler: projectController.delete.bind(projectController)
-  // },
 ];
 
 export { routes };
